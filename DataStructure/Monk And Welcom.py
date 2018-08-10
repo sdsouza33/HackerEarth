1a47c8a099dba835c02a4429e63c7c075ea3168d
sdsouza33 <dsouzasunnny133@gmail.com>
2018-08-10 20:33:35 +0530

    Monk and welcome

--- DataStructure/Monk And Welcom.py
+++ DataStructure/Monk And Welcom.py
@@ -1,6 +1,7 @@
 n=int(input())
 a=[int(i)for i in input().split()]
 b=[int(i)for i in input().split()]
-c=[x+y for x,y in zip(a,b)]
+
+c=[i+j for i,j in zip(a,b)]
 for i in c:
-    print(i , end=" ")
+    print(i ,end=" ")
